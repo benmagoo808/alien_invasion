8788429588ba1dcaa5f3e1756bdc437bee1f7ddd
Ben McGauhey <benmagoo@gmail.com>
2017-03-02 15:12:15 -0800

    limits movement to screen boundaries

--- settings.py
+++ settings.py
@@ -3,7 +3,10 @@ class Settings():
 
     def __init__(self):
         """ Initialize the screen settings """
-        #Screen Settings
+        # Screen Settings
         self.screen_width = 1200
         self.screen_height = 680
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
