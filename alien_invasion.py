f9f64b7b74addfbd9379c39c6b05ef8e0c8aeacf
Ben McGauhey <benmagoo@gmail.com>
2017-03-02 14:50:49 -0800

    Factored functions into game_functions and allowed for movement to the right of the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
-import sys
-
 import pygame
 
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -13,18 +13,14 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Make a ship
+    ship = Ship(screen)
+
     # Start the main loop of the game
     while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
-        # Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
 
 run_game()
