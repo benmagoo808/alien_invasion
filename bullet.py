6b699d11c98a1eef3e3e0c93fd729b63500fc0d8
Ben McGauhey <benmagoo@gmail.com>
2017-03-06 11:14:17 -0800

    refactored bullet functions to game functions

--- bullet.py
+++ bullet.py
@@ -1,11 +1,11 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Bullet(Sprite)
+class Bullet(Sprite):
     """" Create a class for bullets fired from the ship """
     def __init__(self, ai_settings, screen, ship):
         """ Create a bullet object at the ships present position """
-        super(Bullet, self).__init__()
+        super().__init__()
         self.screen = screen
 
         # Create a bullet rect at (0, 0) and then set correct position
@@ -18,4 +18,15 @@ class Bullet(Sprite)
         self.y = float(self.rect.y)
 
         self.color = ai_settings.bullet_color
-        self.speed = ai_settings.bullet_speed_factor
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """ Move the bullet up the screen """
+        # Update the decimal value of the bullet
+        self.y -= self.speed_factor
+        # Update the rect position
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """ Draw the bullet to the screen """
+        pygame.draw.rect(self.screen, self.color, self.rect)
