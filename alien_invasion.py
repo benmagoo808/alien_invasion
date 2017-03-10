ef0a953d0f7a9b2a39d98402798e411be63d2299
Ben <benmagoo@gmail.com>
2017-03-09 17:15:03 -0800

    draw scoreboard and init score

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 import game_functions as gf
 
 
@@ -19,8 +20,9 @@ def run_game():
     # Make the play button
     play_button = Button(ai_settings, screen, "Play")
 
-    # Create an instance to store game settings
+    # Create an instance to store game stats and create a scoreboard
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # Make a ship a group for the bullets and a group for the aliens
     ship = Ship(ai_settings, screen)
@@ -40,7 +42,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets,
                          play_button)
 
 
--- game_functions.py
+++ game_functions.py
@@ -74,7 +74,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship,
         ship.center_ship()
 
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets,
                   play_button):
     """ Update images on the screen and flip to the new screen """
     # Redraw the screen during each pass through the loop
@@ -85,6 +85,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets,
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the scoreboard
+    sb.show_score()
 
     # Draw the play button if the game is inactive
     if not stats.game_active:
